4a52dba83f5731a029efd1a2202ff96af17f43a7
farrasmf <farrasmuhammadfurqon@gmail.com>
2023-12-06 10:44:35 +0700

    API docum

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swagger = require("./config/swagger");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 const router = require("./routers/router");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
+
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
+
+// swagger config
+const specs = swaggerJsdoc(swagger.swaggerOption)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+// end swagger config
+
 app.use("/", router);
 
 app.listen(port, () => console.log(`Server running at port ${port}`));
